80287ebe06e28042a8169ad2bb25f19789e9543c
sampxcs <ianrosales634@gmail.com>
2022-11-21 19:41:37 -0300

    last commit

--- utils/connectMongo.js
+++ utils/connectMongo.js
@@ -1,7 +1,11 @@
 import mongoose from 'mongoose'
 
+console.log('antes de la variable de entorno de connect mongo')
+console.log(process.env)
 const { MONGO_DB_URI } = process.env
 const connectionString = MONGO_DB_URI
+
+console.log('despues')
 
 // conection to MongoDB
 const connectMongo = async () => {
